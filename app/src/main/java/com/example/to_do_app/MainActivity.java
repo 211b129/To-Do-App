024da206ea36951ba11722738c686146610e17b6
Harsh Sharma <hs7898753@gmail.com>
2023-05-10 01:19:33 +0530

    Added Text view and some more but some problem in UI we are facing.

--- app/src/main/java/com/example/to_do_app/MainActivity.java
+++ app/src/main/java/com/example/to_do_app/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.to_do_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton mFab;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        mFab = findViewById(R.id.floatingActionButton);
+        firestore = FirebaseFirestore.getInstance();
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager() , AddNewTask.TAG);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/to_do_app/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.to_do_app;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
